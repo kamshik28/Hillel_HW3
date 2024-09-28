aae51673069bf0f98119ac3dc44efc7da18dda6a
kamshik28 <dmytriikaminskyi@gmail.com>
2024-09-28 17:10:01 +0200

    Add Celsius to Fahrenheit

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,12 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter app - ver 1.0");
         double F = 98.6;
-        double celsius = FtoC(F);
-        System.out.println(F + " Fahrenheit is " + celsius + " Celsius.");
+        double C = FtoC(F);
+        System.out.println(F + " Fahrenheit is " + C + " Celsius.");
+
+        C = 37;
+        F = CtoF(C);
+        System.out.println(C + " Celsius is " + F + " Fahrenheit.");
+
 
     }
     public static double FtoC(double F) {
         return (F - 32) * 5/9;
+    }
+    public static double CtoF(double C) {
+        return (C * 9/5) + 32;
     }
 
 }
